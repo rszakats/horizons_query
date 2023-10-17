be1ba807c36de6049a1fae6787d2219bf7ef3df8
Szakáts Róbert <rszakats@gmail.com>
2023-10-17 14:42:56 +0200

    Rewrited some code. Added all 3 query modes.

--- horizons_query.sh
+++ horizons_query.sh
@@ -15,7 +15,9 @@ bold='\033[1m'
 baseurl="https://ssd.jpl.nasa.gov/api/horizons.api?OBJ_DATA='YES'&MAKE_EPHEM='YES'" # format=text
 tmpdir="/tmp/${USER}"
 mkdir -p ${tmpdir}
-externals="awk grep curl wget"
+externals="awk grep curl wget wc"
+defout="horizons_result.csv"
+csvformat="YES"
 
 argtable="
 format format
@@ -61,23 +63,139 @@ tca3sglimit TCA3SG_LIMIT
 calimsb CALIM_SB
 calimpl CALIM_PL
 "
-# e=($(echo -e "${argtable}" | grep starttime))
-# echo ${e[1]}
+aargtable=($argtable)
+
 # Functions
 
-function help {
+help () {
     echo -e "${bold}Example usage:${normal}"
 
 }
-export -f help
 
-function check_command {
+check_command () {
     args=($@)
     command -v "${args[0]}" >/dev/null 2>&1 || { echo -e >&2 "${red}The script requires ${bold}${green}"${args[0]}"${normal}${red} but it's not installed.  Aborting.${normal}"; exit 1; }
 }
-export -f check_command
 
-function get_HGA {
+process_input_csv () {
+   args=($@)
+   infile="${args[0]}"
+   header=($(awk -F ',' '!/^#/ && NR==1 {print $0}' ${wdir}/${infile} | sed s/,/\ /g))
+   ncol=${#header[@]}
+   lc=0
+   while read line
+   do
+      echo "Processing line : $line"
+      # line=($(echo ${line} | sed s/,/\ /g))
+      u=""
+      for (( C=0; C<=${ncol}-1; C+=1 )); do
+         l=$(echo $line | awk -v C=${C} -F ',' '{print $(C+1)}')
+         name=($(echo -e "${argtable}" | grep -w ${header[$C]}))
+         name=${name[1]}
+         if [ ! -z "${!header[$C]}" ];then
+            echo -e ${red}"Warning! Duplicate! Deleting value${normal} ${bold}"${!header[$C]}" ${normal}for${bold} ${name}"${normal}
+            unset "${header[$C]}"
+         fi
+         u=${u}"&${name}=${l}"
+      done
+      # echo "url: "${u}
+      len=$(echo ${#aargtable[@]}-1 | bc)
+      url=""
+      for (( C=0; C<=${len}; C+=2 )); do
+         a=$(echo ${aargtable[$C]})
+         if [ ! -z "${!a}" ];then
+               name=($(echo -e "${argtable}" | grep -w ${a}))
+               name=${name[1]}
+               url=${url}"&"$(echo $name"="${!a})
+         fi
+      done
+      query=$(echo ${baseurl}${url}${u} | sed s/\;/\%3B/g)
+      # echo $query 
+      wget -q "$query" -O ${tmpdir}/${outfile}
+      HGA=($(get_HGA ${tmpdir}/${outfile}))
+      header0=$(grep -B 2 SOE  ${tmpdir}/${outfile} | head -n 1 |\
+            awk '{print "target, H, G, albedo, ",$0}')
+      if [ ${lc} == 0 ];then
+         echo ${header0} > ${wdir}/${outfile}
+      fi
+      target=$(get_target ${tmpdir}/${outfile})
+      s1=$(grep SOE -n ${tmpdir}/${outfile}| awk -F ':' '{print $1}')
+      s2=$(grep EOE -n ${tmpdir}/${outfile}| awk -F ':' '{print $1}')
+      if [ -z "${s1}" ] || [ -z "${s2}" ]; then
+         echo -e ${red}"Warning! No data found!"${normal}
+         echo -e ${bold}"Please check"${normal}${green} ${tmpdir}/${outfile}${normal} "for errors!" ; exit 1
+      fi
+      echo "$(head -n $(echo ${s2}-1 | bc) ${tmpdir}/${outfile} | tail -n $(echo ${s2}-${s1}-1 | bc) |\
+            awk -v target="${target}" -v H=${HGA[0]} -v G=${HGA[1]} -v albedo=${HGA[2]} '{print target,",",H,","G,",",albedo",",$0}')" >> ${wdir}/${outfile}
+      ((lc+=1))
+   done < <(awk -F ',' '!/^#/ && NR!=1 {print $0}' ${wdir}/${infile})
+
+}
+
+process_query () {
+   url=""
+   len=$(echo ${#aargtable[@]}-1 | bc)
+
+   for (( C=0; C<=${len}; C+=2 )); do
+      a=$(echo ${aargtable[$C]})
+      if [ ! -z "${!a}" ];then
+            name=($(echo -e "${argtable}" | grep -w ${a}))
+            name=${name[1]}
+            url=${url}"&"$(echo $name"="${!a})
+      fi
+   done
+   query=$(echo ${baseurl}${url} | sed s/\;/\%3B/g)
+   # echo $query 
+   wget -q "$query" -O ${tmpdir}/${outfile}
+   HGA=($(get_HGA ${tmpdir}/${outfile}))
+   header=$(grep -B 2 SOE  ${tmpdir}/${outfile} | head -n 1 |\
+         awk '{print "target, H, G, albedo, ",$0}')
+   target=$(get_target ${tmpdir}/${outfile})
+   s1=$(grep SOE -n ${tmpdir}/${outfile}| awk -F ':' '{print $1}')
+   s2=$(grep EOE -n ${tmpdir}/${outfile}| awk -F ':' '{print $1}')
+   if [ -z "${s1}" ] || [ -z "${s2}" ]; then
+      echo -e ${red}"Warning! No data found!"${normal}
+      echo -e ${bold}"Please check"${normal}${green} ${tmpdir}/${outfile}${normal} "for errors!" ; exit 1
+   fi
+   echo ${header} > ${wdir}/${outfile}
+   echo $(head -n $(echo ${s2}-1 | bc) ${tmpdir}/${outfile} | tail -n $(echo ${s2}-${s1}-1 | bc) |\
+            awk -v target="${target}" -v H=${HGA[0]} -v G=${HGA[1]} -v albedo=${HGA[2]} '{print target,",",H,","G,",",albedo",",$0}' >> ${wdir}/${outfile})
+}
+
+get_target () {
+   args=($@)
+   infile="${args[0]}"
+   target=$(grep "Target body name" ${infile} | sed s/"Target body name:"//g)
+   target=${target%"{"*}
+   echo ${target} | awk '{$1=$1;print}'
+}
+
+use_fileapi () {
+   args=($@)
+   infile="${args[0]}"
+   check=$(grep "!\$\$SOF" ${infile}|  wc -l | awk '{print $1}')
+   if [ "${check}" == 0 ]; then
+      echo -e ${red}"Warning! Wrong input file format! Exiting."${normal}; exit 1
+   fi
+   curl -s -F format=text -F input=@${infile} https://ssd.jpl.nasa.gov/api/horizons_file.api >  ${tmpdir}/results.txt
+   size=$(wc -c ${tmpdir}/results.txt | awk '{print $1}')
+   if [ "${size}" == 0 ];then
+      echo -e ${red}"Warning!" Returned file size is zero! Check your settings! Exiting.${normal} ; exit 1
+   fi
+   HGA=($(get_HGA ${tmpdir}/results.txt))
+   header=$(grep -B 2 SOE  ${tmpdir}/results.txt | head -n 1 |\
+            awk '{print "target, H, G, albedo, ",$0}')
+   target=$(get_target ${tmpdir}/results.txt)
+   isoutfile=$(find ${wdir} -maxdepth 1 -type f -name "${outfile}*"  | wc -l)
+   if [[ "${isoutfile}" > 0 ]];then
+      mv ${wdir}${outfile} ${wdir}${outfile}.${isoutfile}
+   fi
+   echo ${header} > ${wdir}${outfile}
+   sed -n '/SOE/,/EOE/{/SOE/b;/EOE/b;p}' ${tmpdir}/results.txt |\
+   awk -v target="${target}" -v H="${HGA[0]}" -v G="${HGA[1]}" -v albedo="${HGA[2]}" '{print target", ",H", ",G", ",albedo", ",$0}' >> ${wdir}${outfile}
+}
+
+get_HGA () {
     args=($@)
     infile="${args[0]}"
     # echo $infile
@@ -109,11 +227,9 @@ function get_HGA {
     fi
     echo $H $G $A 
 }
-export -f get_HGA
+
 
 # Main script
-
-echo "Running with arguments:" "$@"
 
 # Getting command line arguments
 while [[ $# -gt 0 ]] && [[ "$1" == "--"* ]] ;
@@ -235,68 +351,58 @@ done
 for  ext in ${externals};do
     check_command ${ext}
 done
-if [ -z "${wdir}" ]; then
-    wdir=$(pwd)  # or change this to your desired default directory
-fi
 
-# if [ -z "${infile}" ]; then
-#     echo "No input file specified! Use --help"; exit 1
-# fi
+csvformat='YES' # hardwired for now; TODO: allow JSON format
+
+if [ -z "${wdir}" ]; then
+   wdir=$(pwd)  # or change this to your desired default directory
+   if [ "${verbose}" == "True" ]; then
+      echo "Working directory was not specified. Setting to default."
+      echo "New working directory: ${wdir}"
+   fi
+fi
 
 if [ -z "${format}" ]; then
     format="text"
 fi
 
-if [[ "${usefileapi}" != "True" ]] && [ -z "${command}" ]; then
-    echo -e ${red}"Please specify a target body!"${normal}
-    echo "For details see: https://ssd-api.jpl.nasa.gov/doc/horizons.html#command"
-    echo "Use --help"; exit 1
-fi
-
-if [[ "${usefileapi}" != "True" ]] && [ -z "${ephemtype}" ]; then
-    echo -e ${red}"Please specify a type of ephemeris to generate!"${normal}
-    echo "See here: https://ssd-api.jpl.nasa.gov/doc/horizons.html#ephem_type" ; exit 1
-fi
-
 if [ -z "${outfile}" ]; then
-    outfile='horizons_result.csv'  # Change this to your desired default filename
-fi
-
-
-if [[ "${usefileapi}" != "True" ]] && ([[ "${ephemtype}" == "observer" ]] || [[ "${ephemtype}" == "vectors" ]] || [[ "${ephemtype}" == "elements" ]]); then
-    url=""
-    aargtable=($argtable)
-    len=$(echo ${#aargtable[@]}-1 | bc)
-
-    for (( C=0; C<=${len}; C+=2 )); do
-        a=$(echo ${aargtable[$C]})
-        if [ ! -z "${!a}" ];then
-            name=($(echo -e "${argtable}" | grep -w ${a}))
-            # echo ${name[@]}
-            name=${name[1]}
-            url=${url}"&"$(echo $name"="${!a})
-        fi
-    done
-
-    query=$(echo ${baseurl}${url} | sed s/\;/\%3B/g)
-    echo $query 
-    wget -q "$query" -O ${tmpdir}${outfile}
+   if [ "${verbose}" == "True" ]; then
+      echo "Output file was not specified. Setting to default."
+      echo "New output file: ${defout}"
+   fi
+   outfile=${defout}  # Change this to your desired default filename
 fi
 
 if [[ "${usefileapi}" == "True" ]];then
-    curl -s -F format=text -F input=@${infile} https://ssd.jpl.nasa.gov/api/horizons_file.api >  ${tmpdir}/results.txt
-    HGA=($(get_HGA /tmp/${USER}/results.txt))
-    header=$(grep -B 2 SOE  /tmp/${USER}/results.txt | head -n 1 |\
-                awk '{print "target, H, G, albedo, ",$0}')
-    target=$(grep "Target body name" /tmp/${USER}/results.txt | sed s/"Target body name:"//g)
-    target=${target%"{"*}
-    target=$(echo ${target} | awk '{$1=$1;print}')
-    isoutfile=$(find ${wdir} -maxdepth 1 -type f -name "${outfile}*"  | wc -l)
-    if [[ "${isoutfile}" > 0 ]];then
-        mv ${wdir}${outfile} ${wdir}${outfile}.${isoutfile}
-    fi
-    echo ${header} > ${wdir}${outfile}
-    sed -n '/SOE/,/EOE/{/SOE/b;/EOE/b;p}' /tmp/${USER}/results.txt |\
-    awk -v target="${target}" -v H="${HGA[0]}" -v G="${HGA[1]}" -v albedo="${HGA[2]}" '{print target", ",H", ",G", ",albedo", ",$0}' >> ${wdir}${outfile}
+   if [[ ! -z "${ephemtype}" ]];then
+      echo -e ${red}"Warning!${normal} Using --usefileapi and --ephemtype at the same time is not allowed!"
+      echo "Use --help" ; exit 1
+   fi
+   if [ -z "${infile}" ]; then
+      echo "No input file specified! Use --help"; exit 1
+   fi
+   use_fileapi ${wdir}/${infile}
+else
+
+   if [ -z "${command}" ]; then
+      echo -e ${red}"Please specify a target body!"${normal}
+      echo "For details see: https://ssd-api.jpl.nasa.gov/doc/horizons.html#command"
+      echo "Use --help"; exit 1
+   fi
+
+   if [ -z "${ephemtype}" ]; then
+      echo -e ${red}"Please specify a type of ephemeris to generate!"${normal}
+      echo "See here: https://ssd-api.jpl.nasa.gov/doc/horizons.html#ephem_type"
+      echo "Use --help" ; exit 1
+   fi
+
+   if [[ "${ephemtype}" == "observer" ]] || [[ "${ephemtype}" == "vectors" ]] || [[ "${ephemtype}" == "elements" ]]; then
+      if [ ! -z "${infile}" ];then
+         process_input_csv ${infile}
+      else
+         process_query
+      fi
+   fi
 fi
 echo -e "${green}All done!${normal}"
